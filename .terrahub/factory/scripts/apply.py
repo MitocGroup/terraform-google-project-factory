3c41380d198cd557cf366a21f122b5ad680124e3
root <root@DESKTOP-ECSQPB1.localdomain>
2019-05-21 15:44:27 +0200

    fixed conflicts

--- .terrahub/factory/scripts/apply.py
+++ .terrahub/factory/scripts/apply.py
@@ -27,8 +27,7 @@ def main():
     (result, error) = process.communicate()
 
     rc = process.wait()
-    print(includ)
-    print(result)
+
     if rc != 0:
         print("Error: failed to execute command:")
         print(error)
@@ -38,7 +37,6 @@ def main():
     for (key, val) in json.loads(result).items():
     	for (key_sub, val_sub) in val.items():
             response[key_sub]=val_sub['value']
-
     
     with open('output.json', 'wb') as json_file:
         json_file.write(json.dumps(response).encode("utf-8"))
